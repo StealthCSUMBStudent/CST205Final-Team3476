a517218012f3d83b3cf4377328e84825557cf5f0
JudahSilva <jusilva@csumb.edu>
2024-05-01 13:42:59 -0700

    Testing ai api

--- cst205- Project/app.py
+++ cst205- Project/app.py
@@ -3,6 +3,8 @@ import requests
 
 app = Flask(__name__)
 
+open_ai_key = 'sk-proj-gd0elrXdmLbvGSWqbujDT3BlbkFJI3PTDMXa5lyk6WxD6GWf'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     images = []
@@ -12,3 +14,20 @@ def index():
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/ai')
+def aiGeneration():
+    endpoint = 'https://api.openai.com/v1/images/generations'
+    header = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {open_ai_key}'
+    }
+    params = {
+        'model': 'dall-e-3',
+        'prompt': 'a white fox',
+    }
+
+    response = requests.post(endpoint, json=params, headers=header)
+
+    print(response.json())
+    return render_template('index.html')
