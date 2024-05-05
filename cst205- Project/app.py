f180d9ca16658f9db804adecd9365d1189f145b2
JudahSilva <jusilva@csumb.edu>
2024-05-04 20:12:54 -0700

    Add AI image api and display results on a test page

--- cst205- Project/app.py
+++ cst205- Project/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
 import requests
 
 app = Flask(__name__)
@@ -15,20 +16,120 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 @app.route('/ai')
+def route():
+    print('working')
+    return render_template('ai.html')
+
+@app.route('/api/aiImages')
 def aiGeneration():
-    endpoint = 'https://api.openai.com/v1/images/generations'
-    header = {
-        # 'Content-Type': 'application/json',
-        'Authorization': f'Bearer {project_key}',
-        # 'OpenAI-Organization': 'org-2bHSR4U2Px92fzbE4ITpLcYm',
-        # 'OpenAI-Project': project_key
+    url = "https://stable-diffusion9.p.rapidapi.com/generate"
+
+    payload = {
+        "prompt": "dogs",
+        "style": "manga",
+        "seed": 2
     }
-    params = {
-        'model': 'dall-e-2',
-        'prompt': 'a white fox',
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "8867e01260msha8bab1ebf526c14p17d658jsn7f3f3686d05d",
+        "X-RapidAPI-Host": "stable-diffusion9.p.rapidapi.com"
     }
 
-    response = requests.post(endpoint, json=params, headers=header)
+    images = []
+    for i in range(0, 2):
+        response = requests.post(url, json=payload, headers=headers)
 
-    print(response.json())
-    return render_template('index.html')
+        if response.status_code == 200:
+            try:
+                images.append(response.json()['url'])
+            except:
+                print('didn\'t work')
+        else:
+            print(f'Error: {response.status_code} code from API')
+
+    if len(images) != 0:
+        return jsonify(images)
+    else:
+        return None
+
+
+"""
+0:
+name:"cinematic"
+1:
+name:"photographic"
+2:
+name:"anime"
+3:
+name:"manga"
+4:
+name:"digital-art"
+5:
+name:"pixel-art"
+6:
+name:"fantasy-art"
+7:
+name:"neonpunk"
+8:
+name:"3d-model"
+9:
+name:"analog"
+10:
+name:"comic-book"
+11:
+name:"craft-clay"
+12:
+name:"enhance"
+13:
+name:"isometric"
+14:
+name:"line-art"
+15:
+name:"lowpoly"
+16:
+name:"origami"
+17:
+name:"pixel-art"
+18:
+name:"texture"
+19:
+name:"watercolor"
+20:
+name:"artstyle-hyperrealism"
+21:
+name:"paper-quilling"
+22:
+name:"photo-hdr"
+23:
+name:"horror"
+24:
+name:"dreamscape"
+25:
+name:"game-bubble-bobble"
+26:
+name:"game-cyberpunk-game"
+27:
+name:"game-fighting-game"
+28:
+name:"game-gta"
+29:
+name:"game-mario"
+30:
+name:"game-minecraft"
+31:
+name:"game-pokemon"
+32:
+name:"game-retro-arcade"
+33:
+name:"game-retro-game"
+34:
+name:"game-rpg fantasy-game"
+35:
+name:"game-strategy-game"
+36:
+name:"game-streetfighter"
+37:
+name:"game-zelda"
+38:
+name:"furry"
+"""
