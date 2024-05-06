4a8f762e62b1c4992c3935203bb0bf77ef970902
niklopez <112145532+niklopez@users.noreply.github.com>
2024-05-06 13:06:59 -0700

    API and Image Display, Image keywords

--- cst205- Project/app.py
+++ cst205- Project/app.py
@@ -3,12 +3,36 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# Your Pixabay API key
+API_KEY = '43673995-6b601f29737528af485dab3da'
+SEARCH_TERM = 'germany'
+
+@app.route('/')
 def index():
     images = []
-    if request.method == 'POST':
-        author = request.form.get('author')
-        response = requests.get('https://picsum.photos/v2/list')
-    return render_template('index.html')
+    unique_tags = []  
+
+    author = request.args.get('author') 
+
+    if author:
+        response = requests.get(f"https://pixabay.com/api/?key={API_KEY}&q={author}")
+        if response.status_code == 200:
+            data = response.json()
+            print(data)  
+            for image in data['hits']:
+                images.append(image['webformatURL'])
+                tags = image['tags'].split(',')
+                for tag in tags:
+                    tag = tag.strip()
+                    if tag not in unique_tags:
+                        unique_tags.append(tag)
+            print(images)  
+            print(unique_tags)  
+        else:
+            print("Failed to retrieve data from Pixabay API")
+    
+    return render_template('index.html', images=images, unique_tags=unique_tags)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
